816d69747efd1bf18ad661203caac0ee62ab6eb3
Nomfundo <nomfundotswarelo@gmail.com>
2023-04-14 11:21:09 +0200

    removed the old payment function

--- Order_Function.py
+++ Order_Function.py
@@ -64,7 +64,7 @@ def take_order():
     print(f"Total cost: {total_cost:.2f}")
 
 
-take_order()
+# 2()
 
 
  
@@ -74,7 +74,7 @@ def cancel_order():
     onumber = input("Plesae give us an order number")
     conumber = onumber +  'cancel'
     # Read in the file
-    with open('/Users/damacm182_/Desktop/orders.csv', 'r') as file :
+    with open('order.txt', 'r') as file :
         filedata = file.read()
     
 
@@ -83,12 +83,12 @@ def cancel_order():
     
 
     # Write the file out again
-    with open('/Users/damacm182_/Desktop/orders.csv', 'w') as file:
+    with open('order.txt', 'w') as file:
         file.write(filedata)
 
 
 def view_cancelled_orders():
-    with open(r'/Users/damacm182_/Desktop/orders.csv', 'r') as fp:
+    with open(r'order.txt', 'r') as fp:
         # read all lines in a list
         lines = fp.readlines()
         for line in lines:
@@ -97,51 +97,51 @@ def view_cancelled_orders():
                 print(line)
 
 
-orderc = input("Please select an option: 1- Cancelling Order, 2- View Cancelled orders")
+# orderc = input("Please select an option: 1- Cancelling Order, 2- View Cancelled orders")
 
-if orderc == "1":
-    cancel_order()
-    print("Order cancelled succesfully")
-elif orderc == "2":
-    view_cancelled_orders()
-else:
-    print("Invalid selection")
-    exit()
+# if orderc == "1":
+#     cancel_order()
+#     print("Order cancelled succesfully")
+# elif orderc == "2":
+#     view_cancelled_orders()
+# else:
+#     print("Invalid selection")
+#     exit()
    
 
 
-# ask the user if they want to edit their order
-while True:
-    answer = input("Would you like to edit your order? (Y/N): ")
-    if answer == "Y":
+# # ask the user if they want to edit their order
+# while True:
+#     answer = input("Would you like to edit your order? (Y/N): ")
+#     if answer == "Y":
         
-# give the user options to add or remove an item
-        while True:
-            edit_answer = input("What would you like to do? Add or Remove an item? (Add/Remove): ")
-            if edit_answer == "Add":
-                item_to_add = input("Please enter the item you would like to Add: ")
-                if item_to_add == "Burger":  
-                    burger_counter=burger_counter+1
-                print(f"{item_to_add} has been added to your order.")
+# # give the user options to add or remove an item
+#         while True:
+#             edit_answer = input("What would you like to do? Add or Remove an item? (Add/Remove): ")
+#             if edit_answer == "Add":
+#                 item_to_add = input("Please enter the item you would like to Add: ")
+#                 if item_to_add == "Burger":  
+#                     burger_counter=burger_counter+1
+#                 print(f"{item_to_add} has been added to your order.")
                 
-                break
-# remove the item from the order list
-            elif edit_answer == "Remove":
-                item_to_remove = input("Please enter the item you would like to remove: ")
-                if item_to_remove == "Burger":
-                    burger_counter=burger_counter-1
-                print(f"{item_to_remove} has been removed from your order.")
-                break
-            else:
-                print(f"Sorry, {Myorderfood} is not in your order.")
-        else:
-                print("Invalid input. Please enter 'Add' or 'Remove'.")
+#                 break
+# # remove the item from the order list
+#             elif edit_answer == "Remove":
+#                 item_to_remove = input("Please enter the item you would like to remove: ")
+#                 if item_to_remove == "Burger":
+#                     burger_counter=burger_counter-1
+#                 print(f"{item_to_remove} has been removed from your order.")
+#                 break
+#             else:
+#                 print(f"Sorry, {Myorderfood} is not in your order.")
+#         else:
+#                 print("Invalid input. Please enter 'Add' or 'Remove'.")
     
-    elif answer == "N":
-        print("Thank you for your order.")
-        break
-    else:
-        print("Invalid input. Please enter 'Y' or 'N'.")
+#     elif answer == "N":
+#         print("Thank you for your order.")
+#         break
+#     else:
+#         print("Invalid input. Please enter 'Y' or 'N'.")
         
         
         
@@ -157,14 +157,18 @@ def search():
             if order_num in line:
                 print(line)
 
-search()
+# search()
 
 
 
 def cash_payment(amount):
+
     print("You have chosen to pay by cash.")
     print(f"The total amount to be paid is R{amount:.2f}.")
+    
     cash = float(input("Enter amount paid: "))
+    onumber = input("Plesae give us an order number")
+
     while cash < amount:
         print(" Your money is short, please add more or cancel the order.")
         cash = float(input("Enter amount paid: "))
@@ -173,40 +177,71 @@ def cash_payment(amount):
     print("Payment successful")
     print("Thank you for your payment!")
     
+    with open('order.txt','r') as f:
+        newlines = []
+        for line in f:
+            if onumber in line:
+                newlines.append(line.replace('not paid', 'paid'))
+            else:
+                newlines.append(line)
+
+    with open('order.txt', 'w') as f:
+        for line in newlines:
+            f.write(line)
+    
+    card_payment()
     
 # function for card payment
 def card_payment(amount):
     print("You have chosen to pay by card.")
+    onumber = input("Please give me your order number:")
+    amount = float(input("Please enter the amount to be paid:"))
+    acc_num = input("Please enter acc number : ")
     pin = input("Please enter your PIN: ")
+  
 
-    with open("/Users/damacm187/Documents/user.txt", "r") as f:
-        lines = f.readlines()
-        user_pin = lines[1][:4]   # extract the first 4 characters of the second line
-        balance = float(lines[1][4:13])   # extract characters 5-13 of the second line and convert to float
-        card_id = lines[1][6:].strip()  
+    with open('bank.txt','r') as f:
+        newlines_bank = []
+        for line_bank in f:
+            if acc_num and pin in line_bank:
+                balance = line_bank[14:]
+                new_balance = float(balance) -  amount
+                newlines_bank.append(line_bank.replace(str(balance), str(new_balance)))
+                newlines_bank.append("\n")
+                
+                with open('order.txt','r') as f:
+                    newlines = []
+                    for line in f:
+                        if onumber in line:
+                            newlines.append(line.replace('not paid', 'paid'))
+                        else:
+                            newlines.append(line)
 
-    if pin == user_pin:
-        print("PIN verification successful.")
-        if amount <= balance:
-            # update user balance in file
-            with open("/Users/damacm187/Documents/user.txt", "w") as f:
-                f.write(str(balance - amount))
-            print(f"Payment successful. Your remaining balance is R{balance - amount:.2f}.")
-        else:
-            print("Error: Insufficient balance.")
+                with open('order.txt', 'w') as f:
+                    for line in newlines:
+                        f.write(line)
+                 
+                
+            else:
+                newlines_bank.append(line_bank)
+
+    with open('bank.txt', 'w') as f:
+        for line in newlines_bank:
+            f.write(line)
+            
+    card_payment() 
+
+# main for payment functions
+def payment_option():
+    payment_option = cash_payment()
+    payments= input("Please choose a payment option (cash or card): ")
+    amount = float(input("Please enter the amount to be paid:"))
+
+    if payment_option == "cash":
+            cash_payment(amount)
+    elif payment_option == "card":
+            card_payment(amount)
     else:
-        print("Error: Incorrect PIN.")
-
-
-
-# main program
-amount = float(input("Please enter the amount to be paid: "))
-
-payment_option = input("Please choose a payment option (cash or card): ")
-
-if payment_option == "cash":
-    cash_payment(amount)
-elif payment_option == "card":
-    card_payment(amount)
-else:
-    print("Error: Invalid payment option.")
+         print("Error: Invalid payment option.")
+         
+payment_option()
