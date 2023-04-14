7565af8fb884bf8ae992a61cde2e4efb39776885
marcusmk27@gmail.com <marcusmk27@gmail.com>
2023-04-14 12:35:06 +0200

    created main menu function

--- Order_Function.py
+++ Order_Function.py
@@ -50,96 +50,10 @@ def take_order():
 
     print(f"Total cost: {total_cost:.2f}")
 
-
-# 2()
-
-
- 
-    
-<<<<<<< HEAD
-def cancel_order():
-    
-    onumber = input("Plesae give us an order number")
-    conumber = onumber +  'cancel'
-    # Read in the file
-    with open('order.txt', 'r') as file :
-        filedata = file.read()
-    
-
-    # Replace the target string
-    filedata = filedata.replace(onumber,conumber )
-    
-
-    # Write the file out again
-    with open('order.txt', 'w') as file:
-        file.write(filedata)
+    payment_option()
 
 
-def view_cancelled_orders():
-    with open(r'order.txt', 'r') as fp:
-        # read all lines in a list
-        lines = fp.readlines()
-        for line in lines:
-            # check if string present on a current line
-            if 'cancel' in line[0:10] :
-                print(line)
 
-
-# orderc = input("Please select an option: 1- Cancelling Order, 2- View Cancelled orders")
-
-# if orderc == "1":
-#     cancel_order()
-#     print("Order cancelled succesfully")
-# elif orderc == "2":
-#     view_cancelled_orders()
-# else:
-#     print("Invalid selection")
-#     exit()
-   
-
-
-# # ask the user if they want to edit their order
-# while True:
-#     answer = input("Would you like to edit your order? (Y/N): ")
-#     if answer == "Y":
-
-# ask the user if they want to edit their order
-while True:
-    answer = input("Would you like to edit your order? (Y/N): ")
-    if answer == "Y":
-
-        
-# # give the user options to add or remove an item
-#         while True:
-#             edit_answer = input("What would you like to do? Add or Remove an item? (Add/Remove): ")
-#             if edit_answer == "Add":
-#                 item_to_add = input("Please enter the item you would like to Add: ")
-#                 if item_to_add == "Burger":  
-#                     burger_counter=burger_counter+1
-#                 print(f"{item_to_add} has been added to your order.")
-                
-#                 break
-# # remove the item from the order list
-#             elif edit_answer == "Remove":
-#                 item_to_remove = input("Please enter the item you would like to remove: ")
-#                 if item_to_remove == "Burger":
-#                     burger_counter=burger_counter-1
-#                 print(f"{item_to_remove} has been removed from your order.")
-#                 break
-#             else:
-#                 print(f"Sorry, {Myorderfood} is not in your order.")
-#         else:
-#                 print("Invalid input. Please enter 'Add' or 'Remove'.")
-    
-#     elif answer == "N":
-#         print("Thank you for your order.")
-#         break
-#     else:
-#         print("Invalid input. Please enter 'Y' or 'N'.")
-        
-        
-        
-        
 # Marcus cancel
 # here is the code for the search function
         
@@ -151,17 +65,24 @@ def search():
             if order_num in line:
                 print(line)
 
-# search()
 
 # Cancel Order
  
-    
+def view_cancelled_orders():
+    print("\nHere is a list of all cancelled orders:")
+    with open('order.txt', 'r') as fp:
+        for line in fp:
+            # check if string present on a current line
+            if 'cancelled' in line :
+                print(line)
+                
+                
 def cancel_order():
     newlines = []
 
     onumber = input("Plesae give us an order number")
     # Read in the file
-    with open('/Users/damacm182_/Documents/orders.txt', 'r') as file :
+    with open('order.txt', 'r') as file :
         for line in file:
             if onumber in line:
                if "cancelled" in line:
@@ -170,7 +91,7 @@ def cancel_order():
                     print("Order cancelled successfully")
                     newlines.append(line.replace('pending', 'cancelled'))
                else:
-                    print("cannot cancel,order already cancelled")
+                    print("cannot cancel,order already out")
                     newlines.append(line)
             else:
                 newlines.append(line)
@@ -179,25 +100,7 @@ def cancel_order():
             with open('order.txt', 'w') as f:
                     for line in newlines:
                         f.write(line)
-
-    
-
-
-def view_cancelled_orders():
-    print("\nHere is a list of all cancelled orders:")
-    with open(r'/Users/damacm182_/Documents/orders.txt', 'r') as fp:
-        for line in fp:
-            # check if string present on a current line
-            if 'cancelled' in line :
-                print(line)
-                
-                
-cancel_order()
-
-
-orderc = input("Please select an option: 1- Cancelling Order, 2- View Cancelled orders")
-
-
+    # view_cancelled_orders()
 
 def cash_payment(amount):
 
@@ -227,7 +130,6 @@ def cash_payment(amount):
         for line in newlines:
             f.write(line)
     
-    card_payment()
     
 # function for card payment
 def card_payment(amount):
@@ -267,12 +169,9 @@ def card_payment(amount):
         for line in newlines_bank:
             f.write(line)
             
-    card_payment() 
-
 # main for payment functions
 def payment_option():
-    payment_option = cash_payment()
-    payments= input("Please choose a payment option (cash or card): ")
+    payment_option =  input("Please choose a payment option (cash or card): ")
     amount = float(input("Please enter the amount to be paid:"))
 
     if payment_option == "cash":
@@ -280,24 +179,29 @@ def payment_option():
     elif payment_option == "card":
             card_payment(amount)
     else:
-<<<<<<< HEAD
-         print("Error: Invalid payment option.")
-         
-payment_option()
-=======
-        print("Error: Incorrect PIN.")
+            print("Error: Invalid payment option.")
+            
+            
+            
+def main():
+    main_menu = ("take_order", "edit_order", "cancel_order", "search_order")
+    # while True:
+    print()
+    print('choose an option')
+    print('1-Order_food')
+    print('2-Edit_order')
+    print('3-Cancel_order')
+    print('4-Search_order')
 
+    print()
+    user_option = int(input('option: '))
 
-
-# main program
-amount = float(input("Please enter the amount to be paid: "))
-
-payment_option = input("Please choose a payment option (cash or card): ")
-
-if payment_option == "cash":
-    cash_payment(amount)
-elif payment_option == "card":
-    card_payment(amount)
-else:
-    print("Error: Invalid payment option.")
->>>>>>> fd592d1761d63d26a4c8ee92bbbd099311893149
+    if user_option == 1:
+        take_order()
+    # elif user_option == 2:
+    #     edit_order()
+    elif user_option == 3:
+        cancel_order()
+    elif user_option == 4:
+        search()
+main()
