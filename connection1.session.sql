39e07bd9b3d10cbfc83ad61f9fff2ee969045217
edolly55@gmail.com <edolly55@gmail.com>
2023-05-22 10:08:32 +0200

    cancel UI design

---
+++ connection1.session.sql
@@ -0,0 +1,1 @@
+INSERT INTO orders (order_number, dates, hour, food_name, item_num, order_status, payment_status, total_cost) VALUES('666', '2023-05-22', '10:57', 'Wraps', '2', 'pending', 'Not paid',  '20')
